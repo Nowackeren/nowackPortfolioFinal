4568473bb1acb585898893b490001c20b7343dc0
Nowackeren <Nicklasnwck@gmail.com>
2024-10-22 13:16:23 +0200

    faerdigitink

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
-const hamburgerCard = document.querySelector(".hamburger-card");
-const hamburgerIcon = document.querySelector(".hamburger");
-// Henter html elementer og gemmer dem i var
 
 
-hamburgerIcon.addEventListener("click", function () {
-  hamburgerCard.classList.toggle("open");
+/* Scroll til footer */
+document.getElementById('scrollToFooter').addEventListener('click', function(event) {
+  event.preventDefault(); // Forhindrer standardlinkadfærd
+  document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
 });
-// Åbner hamburger når "Clicked" er aktiv
-
-window.addEventListener("click", function (event) {
-  if (
-    !event.target.closest(".hamburger-card") &&
-    hamburgerCard.classList.contains("open")
-  ) {
-    hamburgerCard.classList.remove("open");
-  }
-});
-// Lukker hamburger når enden trykket igen eller udenfor elementet
-
-
-document.addEventListener("touchstart", function() {}, true);
-// Tilhøre ikke hamburger, dette er for at Hover fungere på mobile view. Hover funktion fungere kun på web men med den her virker det også på mobil
